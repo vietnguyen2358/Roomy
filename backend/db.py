155cd1c9af46edc3757ad8d97282744ace3a0efe
Yuuji1140 <vietnguyen2358@gmail.com>
2025-02-15 10:59:28 -0800

    Update database creation method

--- backend/db.py
+++ backend/db.py
@@ -3,20 +3,20 @@ import sqlite3
 def create_table():
     con = sqlite3.connect("database.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE movie(title, year, score)")
-    # cur.executescript("""
-    #                   BEGIN;
-    #                   CREATE TABLE Users(UUID, First name, Last name,Email,Password);
-    #                   CREATE TABLE Groups(Group id(UUID)
-    #                                      List of users (list of user ID),
-    #                                      Zillow link,
-    #                                      List of images(image urls most likely just 3),
-    #                                      Bed count,
-    #                                      Bath count,
-    #                                      Rent,
-    #                                      Address,
-    #                                      Square footage);   
-    #                   COMMIT;    
-    # """)
+    # cur.execute("CREATE TABLE movie(title, year, score)")
+    cur.executescript("""
+                      BEGIN;
+                      CREATE TABLE Users(UUID, First name, Last name,Email,Password);
+                      CREATE TABLE Groups(UUID,
+                                         USERS,
+                                         LINK,
+                                         IMAGES,
+                                         BEDCOUNT,
+                                         BATHCOUNT,
+                                         RENT,
+                                         ADDRESS,
+                                         SQUAREFOOTAGE);   
+                      COMMIT;    
+    """)
     
 create_table()
