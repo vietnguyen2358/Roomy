dd21f411422f7fe70f3830366c749437a27de8e1
Jordandt22 <jordandt123411@gmail.com>
2025-02-15 10:08:19 -0800

    Fixed Client Problem

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <img
+        src={process.env.PUBLIC_URL + "/assets/images/auth-bg.png"}
+        alt="circle"
+        className="home-container__auth-bg"
+      />
+    </div>
+  );
 }
 
 export default Home;
