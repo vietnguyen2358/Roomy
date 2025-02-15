be145474591ec47928ed1061c77b139489753aec
Yuuji1140 <vietnguyen2358@gmail.com>
2025-02-15 15:09:09 -0800

    Added try/except blocks for debugging

--- backend/db.py
+++ backend/db.py
@@ -21,16 +21,17 @@ class Group:
         self.address = address
         self.sqFt = sqFt
 
-def print_table(user):
+def print_user(user):
     headers = ["UUID", "First Name", "Last Name", "Email", "Password"]
     user_data = fetch_user("database.db", user)
 
     print(tabulate(user_data, headers=headers, tablefmt="grid"))
 
 def create_table():
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
-    cur.executescript("""
+    try:
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        cur.executescript("""
                       BEGIN;
                       CREATE TABLE IF NOT EXISTS Users(UUID TEXT PRIMARY KEY UNIQUE,
                                                     FIRST_NAME TEXT,
@@ -47,13 +48,18 @@ def create_table():
                                          SQUAREFOOTAGE INTEGER,
                                          FOREIGN KEY (USERS) REFERENCES Users(UUID));   
                       COMMIT;    
-    """)
+        """)
+    except sqlite3.Error as e:
+        print(f"Table Creation Error: {e}")
+    finally:
+        con.close()
 
 # creates new user 
 def insert_user(file, user):
-    con = sqlite3.connect(file)
-    cur = con.cursor()
-    cur.execute("""
+    try:
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("""
                 INSERT INTO Users VALUES
                     (UUID,
                     First,
@@ -63,12 +69,17 @@ def insert_user(file, user):
                 VALUES (?, ?, ?, ?, ?)
                 """,
                 (user.UUID, user.firstName, user.lastName, user.email, user.password))
+    except sqlite3.Error as e:
+        print(f"Table Insertion Error: {e}")
+    finally:
+        con.close()
     
 # creates a new apt listing
-def add_group(file,group):
-    con = sqlite3.connect(file)
-    cur = con.cursor()
-    cur.execute("""
+def add_group(file, group):
+    try:
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("""
                 INSERT INTO Groups VALUES
                     (UUID, 
                     USERS, 
@@ -82,6 +93,10 @@ def add_group(file,group):
                 VALUES (?,?,?,?,?,?,?,?,?)
                 """,
                 (group.UUID, group.users, group.link, group.images, group.bedCount,group.bathCount ,group.rent ,group.address, group.sqFt))
+    except sqlite3.Error as e:
+        print(f"Group Insertion Error: {e}")
+    finally:
+        con.close()
     
 # add the user into the apt group    
 def insert_user_group(file, user):
@@ -96,9 +111,10 @@ def insert_user_group(file, user):
 
 # update the values of apt
 def update_group(file,group):
-    con = sqlite3.connect(file)
-    cur = con.cursor()
-    cur.execute("""
+    try: 
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("""
                 UPDATE Groups
                 SET 
                 UUID = ?, 
@@ -113,24 +129,38 @@ def update_group(file,group):
             WHERE UUID = ?
                 """,
                 (group.UUID, group.users, group.link, group.images, group.bedCount, group.bathCount, group.rent, group.adress, group.sqFt, group.UUID))
+    except sqlite3.Error as e:
+        print(f"Update Group Error: {e}")
+    finally:
+        con.close()
     
 # get all apts
 def fetch_all_groups(file):
-    con = sqlite3.connect(file)
-    cur = con.cursor()
-    cur.execute("""SELECT * FROM Groups;""")
-    return cur.fetchall()
+    try:
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("""SELECT * FROM Groups;""")
+        return cur.fetchall()
+    except sqlite3.Error as e:
+        print(f"Fetching All Group Error: {e}")
+    finally:
+        con.close()
 
 # get all users
 def fetch_user(file,user):
-    con = sqlite3.connect(file)
-    cur = con.cursor()
-    cur.execute("""
+    try:
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("""
                 SELECT * FROM Users
                 WHERE UUID = ?;
             """, 
             (user.UUID))
-    return cur.fetchall()
+        return cur.fetchall()
+    except sqlite3.Error as e:
+        print(f"Fetch User Error: {e}")
+    finally:
+        con.close()
 
 def verify(file, email, password):
     return cur.execute()
@@ -146,12 +176,17 @@ def fetch_user_groups(file,user,group):
 
 # remove the apt listing
 def remove_group(file, user):
-    con = sqlite3.connect(file)
-    cur = con.cursor()
-    cur.execute("""
+    try:
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("""
                 DELETE FROM Groups
                 WHERE UUID = ?;
                 """,
                 (user.UUID))
+    except sqlite3.Error as e:
+        print(f"Remove Group Error: {e}")
+    finally:
+        con.close()
     
 # create_table()
